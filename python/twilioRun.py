a6abe1baaa7c4b51de02bda395cc6e2bb7e7c830
Anna <annashabayev@gmail.com>
2016-11-06 12:58:12 -0600

    simple reply and response
    Reply and response works now. //TODO: store data

--- python/twilioRun.py
+++ python/twilioRun.py
@@ -56,8 +56,8 @@ def sms():
     r = twiml.Response()
     if '0' in body:
         counter = 0;
-        expires=datetime.utcnow() + timedelta(hours=0)
-        resp.set_cookie('messagecount',value=str(messagecount),expires=0)
+        #expires=datetime.utcnow() + timedelta(hours=0)
+        #resp.set_cookie('messagecount',value=str(messagecount),expires=0)
     if ('hello' in body) and (counter == 1):
         m ="Hello " + name + ", welcome to our app! If you are in need of a " + \
             "doctor's appointment, fill out this simple, school physical-style " +\
@@ -66,9 +66,40 @@ def sms():
         r.message(m)
         #resp = make_response(str(twml))
  
-        #expires=datetime.utcnow() + timedelta(hours=1)
-       # resp.set_cookie('messagecount',value=str(messagecount),expires=expires.strftime('%a, %d %b %Y %H:%M:%S GMT'))
-    if counter == 1:
+    if counter == 2:
+        r.message("What is your weight in pounds?")
+    if counter == 3:
+        r.message("What is your age?") 
+    if counter == 4:
+        r.message("Do you identify as transgender?")   
+    if counter == 5:
+        r.message("Have you been hospitalized recently?")
+    if counter == 6:
+        r.message("Have you had surgery recently?")
+    if counter == 7:
+        r.message("Are you currently taking any medication?") 
+    if counter == 8:
+        r.message("Do you suffer from chronic illness?")   
+    if counter == 9:
+        r.message("Do you have any allergies?")
+    if counter == 10:
+        r.message("Do you have asthma?")
+    if counter == 11:
+        r.message("Do you have heart disease?") 
+    if counter == 12:
+        r.message("If you menstruate, how old were you when you had your first period?")   
+    if counter == 13:
+        r.message("If you menstruate, when was your last period?")
+    if counter == 14:
+        r.message("For each of these areas, have you experienced: coughing, sneezing, nausea, dizziness, vision issues, hearing issues, vomiting, breathing issues, congestion, urination issues, defecation issues, fever, shaking, chills, heat, exhaustion, insomnia, bloating, runny nose, abnormal mucus color, sore throat, body aches, swollen lymph nodes, mood swings, difference in appetite, weight gain, abnormal sweating" +\
+                "Have you experienced the following in your head?")
+    if counter == 15:
+        r.message("Have you experienced the following in your throat?") 
+    if counter == 16:
+        r.message("Have you experienced the following on your back?")   
+    if counter == 17:
+        r.message("Have you experienced the following in your stomach?")
+    elif counter == 18:
         if 'sniff' in body:
             r.message("You might have the flu.")
         elif 'cold' in body:
@@ -78,13 +109,16 @@ def sms():
             r.message("Fluuuu.")
         elif 'cold' in body:
             r.message("Brrrrrrrrrrr. Very Cold")
-        else:
-            r.message(str(counter))
+        #else:
+            #r.message(str(counter))
 
     message = "".join([name, " has messaged ", request.values.get('To'), " ", 
         str(counter), " times."])
     resp = twilio.twiml.Response()
     resp.sms(message)
+
+    #expires=datetime.utcnow() + timedelta(hours=0)
+    #resp.set_cookie('messagecount',value=str(counter),expires=expires.strftime('%a, %d %b %Y %H:%M:%S GMT'))
 
     print str(r)
     print "OOOO"
@@ -94,7 +128,3 @@ def sms():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-    #https://gist.github.com/devinrader/d50234733b8e059b98b8
-    #https://gist.github.com/devinrader/ba796aa318174958c14c
-    #https://www.twilio.com/docs/quickstart/python/sms/tracking-conversations
